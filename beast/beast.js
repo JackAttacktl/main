71c9503d411254bf9344cf0cf29ed545568ab546
JackAttacktl <48497979+JackAttacktl@users.noreply.github.com>
2023-11-24 14:33:43 -0600

    Meta

--- beast/beast.js
+++ beast/beast.js
@@ -1,32 +1,3 @@
-var images = [];
-function preload() {
-    for (var i = 0; i < arguments.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.arguments[i];
-    }
-}
-
-const beasts = [
-    "beast1.png",
-    "beast2.png",
-    "beast3.png",
-    "beast4.png",
-    "beast5.png",
-    "beast6.png"
-];
-
-beasts.forEach(img => function()
-{
-    preload(img)
-});
-
-const beastimg = document.getElementById('beastimg');
-
-setInterval(function()
-{
-    beastimg.src = beasts[Math.floor(Math.random() * 6)]
-},10)
-
 var audio = new Audio('mus_mrbeastovania.wav');
 audio.loop = true;
 audio.addEventListener("canplaythrough", () => {
