339310f02f909a259b7c45971ea97c87de98f02c
JackAttacktl <48497979+JackAttacktl@users.noreply.github.com>
2023-11-27 20:26:34 -0600

    why are these summeries required sfggnfgdsd

--- beast/beast.js
+++ beast/beast.js
@@ -1,9 +1,9 @@
 var audio = new Audio('mus_mrbeastovania.wav');
 audio.loop = true;
 audio.addEventListener("canplaythrough", () => {
-    audio.play().catch(e => {
-       window.addEventListener('click', () => {
-            audio.play()
-       }, { once: true })
-    })
- });
+   audio.play().catch(e => {
+      window.addEventListener('click', () => {
+         audio.play();
+      }, { once: true })
+   })
+});
