edbf6b7b61f8d1b1ce392fd8a0479909f041543d
JackAttacktl <48497979+JackAttacktl@users.noreply.github.com>
2024-04-03 20:23:12 -0500

    Overhauled member system

--- members/listcreator.js
+++ members/listcreator.js
@@ -1,47 +1,35 @@
-const members = {
-    "Jaxon": {
-        "avatar": "https://cdn.discordapp.com/avatars/859248471839473684/a3b992084a681eb059af82bc715366db.png",
-        "description": "Don't tell rockstar I stole their beta release",
-        "color": "blue",
-        "link": "jaxon/"
-    },
-    "Wizard": {
-        "avatar": "https://cdn.discordapp.com/avatars/803471962084212777/29fbf32c756dd0021a2de5c3c79c9a33.png",
-        "description": "★ :EspressoHappy: Hai I'm DeceasedWizard (Wiz)!",
-        "color": "red",
-        "link": "wizard/"
-    },
-    "Mercedeez": {
-        "avatar": "https://cdn.discordapp.com/avatars/834139398592659466/422c362147040325ebe28ecbb59be1a3.png",
-        "description": "I'M THE TWINKLE",
-        "color": "pink",
-        "link": "mercedeez/"
-    }
-};
+function fetchJSONData(path) {
+    fetch(path)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+              console.log(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+
+const members = fetchJSONData("assets/membersList.json");
 
 const memberobjects = [];
 const zindexes = [];
-const music = new Audio("remix.ogg");
-let couldplay = false;
-let canplay = false;
-music.currentTime = (sessionStorage.getItem("music_playback") || 0);
-music.loop = true;
-music.volume = 0.1;
-let paused = true;
-let ragdollplaying = false;
 
 function load_members() {
     const membercontainer = document.getElementById("membercontainer");
     for (const [username, data] of Object.entries(members)) {
         const a = document.createElement("a");
         a.className = "member";
-        a.href = data.link;
+        a.href = username;
         a.style = "border-color: " + data.color + "; box-shadow: 0px 0px 20px 1px " + data.color + "; z-index: 0"
         const img = document.createElement("img");
-        img.src = data.avatar;
+        img.src = "assets/" + data.avatar_file_name;
         const div = document.createElement("div");
         const h1 = document.createElement("h1");
-        h1.innerText = username;
+        h1.innerText = data.display_name + " (" + data.username + ")";
         const h2 = document.createElement("h2");
         h2.innerText = data.description;
         a.appendChild(img);
@@ -87,57 +75,3 @@ function load_members() {
         }
     });
 }
-
-const intID = setInterval(function() {
-    if (navigator.userActivation.hasBeenActive && couldplay) {
-        clearInterval(intID);
-        music.play();
-        canplay = true;
-        document.getElementById("musicbutton").src = "pause.png";
-        document.getElementById("musicbutton").style = "background-color: white;"
-        paused = false;
-    }
-},0);
-
-music.addEventListener("canplaythrough",function() {
-    couldplay = true;
-});
-
-window.addEventListener("beforeunload",function() {
-    this.sessionStorage.setItem("music_playback",music.currentTime);
-});
-
-function ragdollspam() {
-    const elm1 = memberobjects[0];
-    const elm2 = memberobjects[1];
-    if (memberobjects.length > 2) {
-        memberobjects[2].style.zIndex = (memberobjects[2].style.zIndex > 1 ? memberobjects[2].style.zIndex : 2)
-    }
-    const spamsound = new Audio("ragdoll spam.mp3");
-    spamsound.volume = 0.2
-    spamsound.oncanplaythrough = function() {
-        spamsound.play();
-        spaminter = setInterval(function() {
-            if (spamsound.ended) {
-                clearInterval(spaminter);
-                ragdollplaying = false;
-            }
-            if (Math.random() <= 0.5) {
-                elm1.style.zIndex = 0
-                elm2.style.zIndex = 1
-            } else {
-                elm1.style.zIndex = 1
-                elm2.style.zIndex = 0
-            }
-        },0);
-    }
-}
-
-setInterval(function() {
-    if (Math.random() <= 0.01) {
-        if (!ragdollplaying && navigator.userActivation.hasBeenActive && memberobjects.length > 1) {
-            ragdollplaying = true;
-            ragdollspam();
-        }
-    }
-},1000);
